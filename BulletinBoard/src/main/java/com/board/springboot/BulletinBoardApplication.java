9d8f49825caacb97c9eaab1fa87ed38feec3afe0
taken10 <30792581+taken10@users.noreply.github.com>
2018-09-18 17:48:12 +0900

    Add files via upload

---
+++ BulletinBoard/src/main/java/com/board/springboot/BulletinBoardApplication.java
@@ -0,0 +1,12 @@
+package com.board.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinBoardApplication.class, args);
+	}
+}
